fe5570f83168583798c6c0751a466394fef5fb14
violette01 <mil250431@gmail.com>
2026-06-07 00:56:37 +0500

    Update admin panel with list_display and search_fields

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import TextSample, LexicalAnalysis, SentimentScore
 
-admin.site.register(TextSample)
-admin.site.register(LexicalAnalysis)
-admin.site.register(SentimentScore)
+
+@admin.register(TextSample)
+class TextSampleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'created_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('title', 'content')
+    ordering = ('-created_at',)
+
+
+@admin.register(LexicalAnalysis)
+class LexicalAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text_sample', 'word_count', 'unique_words', 'flesch_reading_ease')
+    search_fields = ('text_sample__title',)
+
+
+@admin.register(SentimentScore)
+class SentimentScoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text_sample', 'paragraph_index', 'positive_score', 'negative_score', 'compound_score')
+    list_filter = ('compound_score',)
+    search_fields = ('text_sample__title', 'paragraph_text')
